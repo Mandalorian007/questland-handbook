e100aa348061141a83bb174908c39cdcb35e6b3b
mcf <mcf@spotify.com>
2020-02-13 16:40:49 -0500

    standardize a builds file and add a default expansion panel to handle dropping in builds

--- src/pages/ArenaPage.tsx
+++ src/pages/ArenaPage.tsx
@@ -1,191 +1,53 @@
 import * as React from 'react';
+import { Paper } from '@material-ui/core';
+import { BuildExpansionPanel } from '../components/BuildExpansionPanel';
 import {
-  createStyles,
-  ExpansionPanel,
-  ExpansionPanelDetails,
-  ExpansionPanelSummary,
-  Paper,
-  Theme,
-  Typography
-} from '@material-ui/core';
-import { makeStyles } from '@material-ui/core/styles';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+  FireBlaster,
+  IceCannon,
+  ThePAXAntiHealEdition
+} from '../domain/builds';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '100%'
-    },
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-      fontWeight: theme.typography.fontWeightRegular
-    }
-  })
+export const ArenaPage = () => (
+  <Paper>
+    <h1>Dominating in the arena</h1>
+    <p>
+      Questland's arena can at first be extremely intimidating, but it's
+      actually incredibly easy to blast through to the top of the ladder by
+      leveraging some powerful arena builds.
+    </p>
+
+    <h3>Offensive arena setup</h3>
+    <p>
+      Winning when attacking before hitting higher ranks is as simple as
+      starting to use one of the popular builds listed in this website. These
+      should easily carry you up to mid tier arena ranks (1500+) without any
+      issues at all. My personal favorite build fo this is <b>The Turtle</b>. At
+      higher levels things get to be a little trickier due to the presence of
+      super strong anti-healing builds. The builds below should serve you well.
+    </p>
+    <BuildExpansionPanel qlBuild={FireBlaster} />
+    <BuildExpansionPanel qlBuild={IceCannon} />
+    <BuildExpansionPanel qlBuild={ThePAXAntiHealEdition} />
+
+    <h3>Defensive arena setup</h3>
+    <p>
+      Defensive arena setup is really a just a mirror of looking at the most
+      powerful offensive builds in the game. Almost all of them leverage healing
+      for spirit generation and consistent damage output.
+    </p>
+
+    <h3>The meta defense</h3>
+    <p>
+      Basically equip any shield that has "when opponent heals trigger 2 white"
+      Then leverage Sacred Rage or White Witchcraft as a backup. Equip the rest
+      of either "Hectacombus" or "The Turtle" from the popular builds. The
+      entire meta is basically just punish healing while enabling your own dps.
+      This defensive setup will make anyone lower geared, or a bit above your
+      gear level significantly hesitate before engaging you and have a
+      dramatically lower chance of winning. As an alternative for higher ranking
+      players give "The Fire Blaster" a try on defense as well as attac. It is
+      suprisingly strong at punishing the most popular attack builds.
+    </p>
+    <BuildExpansionPanel qlBuild={FireBlaster} />
+  </Paper>
 );
-
-export const ArenaPage = () => {
-  const classes = useStyles();
-
-  return (
-    <Paper>
-      <h1>Dominating in the arena</h1>
-      <p>
-        Questland's arena can at first be extremely intimidating, but it's
-        actually incredibly easy to blast through to the top of the ladder by
-        leveraging some powerful arena builds.
-      </p>
-
-      <h3>Offensive arena setup</h3>
-      <p>
-        Winning when attacking before hitting higher ranks is as simple as
-        starting to use one of the popular builds listed in this website. These
-        should easily carry you up to mid tier arena ranks (1500+) without any
-        issues at all. My personal favorite build fo this is <b>The Turtle</b>.
-        At higher levels things get to be a little trickier due to the presence
-        of super strong anti-healing builds. The builds below should serve you
-        well.
-      </p>
-      <ExpansionPanel>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>
-            The Fire Blaster (Great for heroes with good attack stats)
-          </Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            <h4>Weapons</h4>
-            <ul>
-              <li>Hecatombus</li>
-              <li>Windwolf Shield</li>
-            </ul>
-
-            <h4>Talents</h4>
-            <ul>
-              <li>Inner Fire</li>
-              <li>Chilling Cold</li>
-              <li>Sacred Rage</li>
-            </ul>
-
-            <h4>Playstyle</h4>
-            <p>
-              This build primarily leverages 4R Inner Fire to blast down an
-              opponent. If you happen to have a 4B for free damage & stun feel
-              free to use it, but otherwise your normal tactic will be to use a
-              2W to trigger a heal and start a back and forth chaining mess with
-              your opponent to flood your board with red spirits. Usually after
-              this it only takes 2 or 3 attacks to completely kill your opponent
-              due to Inner Fire's crazy damage output.
-            </p>
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-      <ExpansionPanel>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>
-            The Ice Cannon (Great for heroes with good magic stats)
-          </Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            <h4>Weapons</h4>
-            <ul>
-              <li>Malachite Truncheon</li>
-              <li>Windwolf Shield</li>
-            </ul>
-
-            <h4>Talents</h4>
-            <ul>
-              <li>Inner Fire</li>
-              <li>Chilling Cold</li>
-              <li>White Witchcraft</li>
-            </ul>
-
-            <h4>Playstyle</h4>
-            <p>
-              This build primarily leverages the insanely strong offensive and
-              defensive potential of 4B Chilling Cold. Between stuns and flat
-              out not caring about opponent's shield regeneration you can't get
-              much better than this. Inner fire is more of a placeholder since
-              our opener is typically a 2w to start a chain healing competition
-              to flood our board with blue spirits. After you have the spirits
-              all you need to do is spam 4B until the opponent is dead. Enjoy
-              stunning them and recharging your whole shield.
-            </p>
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-      <ExpansionPanel>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>
-            The PAX anti-heal edition (stronger on magic builds)
-          </Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            <h4>Weapons</h4>
-            <ul>
-              <li>The PAX</li>
-              <li>
-                Depth of Despair (anti-magic) / Thunderbash (anti-physical)
-              </li>
-            </ul>
-
-            <h4>Talents</h4>
-            <ul>
-              <li>BloodLust</li>
-              <li>Chilling Cold</li>
-              <li>Magic Thief</li>
-            </ul>
-
-            <h4>Playstyle</h4>
-            <p>
-              This build has a tough start if you get bad rng, but the goal is
-              to reach 3 intensity and chain like a crazy person from your 4B.
-              For the opener I recommend trying to launch a 4B quickly to get an
-              early stun and buy you some time. Use 1W to generate more blues
-              and 1R to generate your intensity. The moment you hit 3 intensity
-              this build will supply you with easy supply of blue spirits for
-              smashing your opponent's face in with 4Bs. You will immediately
-              disable a ton of defensive builds in arena due to completely
-              ignoring healing. I recommend targeting primarily physical builds
-              with this setup since your only defense is against physical
-              damage.
-            </p>
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-
-      <h3>Defensive arena setup</h3>
-      <p>
-        Defensive arena setup is really a just a mirror of looking at the most
-        powerful offensive builds in the game. Almost all of them leverage
-        healing for spirit generation and consistent damage output.
-      </p>
-
-      <h3>The meta defense</h3>
-      <p>
-        Basically equip any shield that has "when opponent heals trigger 2
-        white" Then leverage Sacred Rage or White Witchcraft as a backup. Equip
-        the rest of either "Hectacombus" or "The Turtle" from the popular
-        builds. The entire meta is basically just punish healing while enabling
-        your own dps. This defensive setup will make anyone lower geared, or a
-        bit above your gear level significantly hesitate before engaging you and
-        have a dramatically lower chance of winning. As an alternative for
-        higher ranking players give "The Fire Blaster" a try on defense. It is
-        suprisingly strong at punishing the most popular attack builds.
-      </p>
-    </Paper>
-  );
-};
--- src/pages/BuildsPage.tsx
+++ src/pages/BuildsPage.tsx
@@ -1,216 +1,24 @@
 import * as React from 'react';
+import { Paper } from '@material-ui/core';
+import { BuildExpansionPanel } from '../components/BuildExpansionPanel';
 import {
-  createStyles,
-  ExpansionPanel,
-  ExpansionPanelDetails,
-  ExpansionPanelSummary,
-  Paper,
-  Theme,
-  Typography
-} from '@material-ui/core';
-import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
-import { makeStyles } from '@material-ui/core/styles';
+  RatchetRush,
+  TheHecatombus,
+  ThePAX,
+  TheTurtle
+} from '../domain/builds';
 
-const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      width: '100%'
-    },
-    heading: {
-      fontSize: theme.typography.pxToRem(15),
-      fontWeight: theme.typography.fontWeightRegular
-    }
-  })
+export const BuildsPage = () => (
+  <Paper>
+    <h1>Popular Builds</h1>
+    <p>
+      This section is dedicated to covering the popular and meta builds in
+      Questland and is a great starting place for new players who simply need to
+      know a good build to get started.
+    </p>
+    <BuildExpansionPanel qlBuild={TheHecatombus} />
+    <BuildExpansionPanel qlBuild={TheTurtle} />
+    <BuildExpansionPanel qlBuild={RatchetRush} />
+    <BuildExpansionPanel qlBuild={ThePAX} />
+  </Paper>
 );
-
-export const BuildsPage = () => {
-  const classes = useStyles();
-
-  return (
-    <Paper>
-      <h1>Popular Builds</h1>
-      <p>
-        This section is dedicated to covering the popular and meta builds in
-        Questland and is a great starting place for new players who simply need
-        to know a good build to get started.
-      </p>
-
-      <ExpansionPanel>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>The Hecatombus</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            <p>
-              This is the defacto go to build for anyone focusing on attack and
-              offers and impressive level of healing and damage.
-            </p>
-
-            <h4>Weapons</h4>
-            <ul>
-              <li>Hecatombus</li>
-              <li>Azazel Shield</li>
-            </ul>
-
-            <h4>Talents</h4>
-            <ul>
-              <li>BloodLust</li>
-              <li>Transcendental Tornado</li>
-              <li>Crest Guardian</li>
-            </ul>
-
-            <h4>Playstyle</h4>
-            <p>
-              Hit 1 white or 1 blue to trigger a heal which will add 2 red to
-              your board from Hecatombus. Repeat this until you have one or more
-              4 red available. Use 4 red which will deal damage and heal you as
-              well as trigger a heal and damage boost from Azazel which procs
-              the 1 white. Since you heal twice it will trigger Hecatombus twice
-              and add 4 red to your board ensuring you will replenish or gain
-              red spirits to continue comboing.
-            </p>
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-      <ExpansionPanel>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>The Turtle</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            <p>
-              This build is the exact opposite of the Hectacombus build and
-              focuses on magic damage first. It's referred to as the turtle
-              because it regenerates a large amount of armor and some health
-              every turn making it extremely difficult to kill.
-            </p>
-
-            <h4>Weapons</h4>
-            <ul>
-              <li>Malachite Truncheon</li>
-              <li>Depth of Despair</li>
-            </ul>
-
-            <h4>Talents</h4>
-            <ul>
-              <li>Inner Fire</li>
-              <li>Chilling Cold</li>
-              <li>Magic Thief</li>
-            </ul>
-
-            <h4>Playstyle</h4>
-            <p>
-              Hit 1 red to trigger a heal which will add 2 blue to your board
-              and generate 1 blue intensity. When you have 3 blue intensity
-              instead of hitting 1 red switch to 1 white to potentially generate
-              4 blue instead of just 2. Once you have one or more 4 blue
-              available, Continuously use 4 blue which will generate significant
-              armour regen. This will also trigger Depth of Despair to proc 1
-              red which will heal you and keep your blue intensity high. Since
-              this build doesn't recover a full 4 blue you can go back to
-              hitting 1 white if you have 3 intensity or 1 red if you don't to
-              recover additional blue spirits.
-            </p>
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-      <ExpansionPanel>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>Ratchet Rush</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            <p>
-              This build is the old attack king build before Hecatombus was
-              released. It is ideal for situations where you need to avoid
-              healing, but still need a strong damage output.
-            </p>
-
-            <h4>Weapons</h4>
-            <ul>
-              <li>Ratchet Hatchet</li>
-              <li>Azazel Shield</li>
-            </ul>
-
-            <h4>Talents</h4>
-            <ul>
-              <li>Inner Fire</li>
-              <li>Faerie Flame</li>
-              <li>Elevation</li>
-            </ul>
-
-            <h4>Playstyle</h4>
-            <p>
-              This build has a slower start than Hecatombus, but it works by
-              spamming 1 blue or 1 white until you have 4 red available. Once
-              you have 4 red available spam 4 red as often as possible. When you
-              use 4 red you will add 1 red to the board and trigger 1 blue and 1
-              white from Ratchet Hachet and Azazels which additionally gives you
-              armour and 2 more reds on the board.
-            </p>
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-      <ExpansionPanel>
-        <ExpansionPanelSummary
-          expandIcon={<ExpandMoreIcon />}
-          aria-controls="panel1a-content"
-          id="panel1a-header"
-        >
-          <Typography className={classes.heading}>The PAX</Typography>
-        </ExpansionPanelSummary>
-        <ExpansionPanelDetails>
-          <Typography>
-            <p>
-              This build is famous for it's insanely high RNG based damage and
-              it's ability to clear almost any level in the campaign when RNG is
-              in your favor.
-            </p>
-
-            <h4>Weapons</h4>
-            <ul>
-              <li>The Pax</li>
-              <li>Depth of Despair</li>
-            </ul>
-
-            <h4>Talents</h4>
-            <ul>
-              <li>Fist of Frenzy</li>
-              <li>Faerie Flame</li>
-              <li>Magic Thief</li>
-            </ul>
-
-            <h4>Playstyle</h4>
-            <p>
-              This build is extremely heavy on chance based triggers, but when
-              everything triggers it can pull some insane dps, but the healing
-              is very unreliable and there is no armor recovery. You open the
-              build with spamming 1 white to generate blue spirits until you
-              have 4 blue available. Then you spam 4 blue as often as possible
-              which will grant you blue intensity and when your blue intensity
-              builds up you have a change to trigger 2 red which can heal you
-              and grant 1 blue to the board. Since both Pax and Depth of Despair
-              are equipped both a 1 white and 1 red will be triggered. 1 red
-              will grant you an additional 1 blue to the board. The 1 white will
-              grant you 1-2 blue to the board and when your intensity reaches
-              level 3 you will also have a high chance at an additional 1 red
-              triggering.
-            </p>
-          </Typography>
-        </ExpansionPanelDetails>
-      </ExpansionPanel>
-    </Paper>
-  );
-};
