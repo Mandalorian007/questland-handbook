da6d56bbec9007d87e209e2a73975b89e1a55eaa
mcf <mcf@spotify.com>
2020-02-12 20:43:01 -0500

    fix react types

--- src/pages/ItemIndexPage.tsx
+++ src/pages/ItemIndexPage.tsx
@@ -17,9 +17,11 @@ import {
 import { loadItems } from '../store/itemActions';
 import TableSortLabel from '@material-ui/core/TableSortLabel';
 import TablePagination from '@material-ui/core/TablePagination';
+import { getComparator, stableSort } from '../lib/sort';
+import { Item } from '../domain/item';
 
 interface ColumnData {
-  id: string;
+  id: keyof Item;
   label: string;
 }
 
@@ -35,44 +37,17 @@ const itemColumns: ColumnData[] = [
   { id: 'health', label: 'Health' }
 ];
 
-// TODO remove any and properly type these
-const descendingComparator = (a: any, b: any, orderBy: any) => {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-};
-
-const getComparator = (order: any, orderBy: any) => {
-  return order === 'desc'
-    ? (a: any, b: any) => descendingComparator(a, b, orderBy)
-    : (a: any, b: any) => -descendingComparator(a, b, orderBy);
-};
-
-const stableSort = (array: any, comparator: any) => {
-  const stabilizedThis = array.map((el: any, index: any) => [el, index]);
-  stabilizedThis.sort((a: any, b: any) => {
-    const order = comparator(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el: any) => el[0]);
-};
-
 export const useSelector: TypedUseSelectorHook<AppState> = useReduxSelector;
 
 export const ItemIndexPage: React.FC<{}> = () => {
   const items = useSelector(state => state.itemState.items);
   const [order, setOrder] = React.useState<'asc' | 'desc'>('asc');
-  const [orderBy, setOrderBy] = React.useState('calories');
+  const [orderBy, setOrderBy] = React.useState<keyof Item>('totalPotential');
   const [page, setPage] = React.useState(0);
 
   const rowsPerPage: number = 10;
 
-  const handleRequestSort = (property: string) => {
+  const handleRequestSort = (property: keyof Item) => {
     const isAsc = orderBy === property && order === 'asc';
     setOrder(isAsc ? 'desc' : 'asc');
     setOrderBy(property);
@@ -114,7 +89,7 @@ export const ItemIndexPage: React.FC<{}> = () => {
             </TableRow>
           </TableHead>
           <TableBody>
-            {stableSort(items, getComparator(order, orderBy))
+            {stableSort(items, getComparator<Item>(order, orderBy))
               .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
               .map((item: any) => (
                 <TableRow key={item.id}>
