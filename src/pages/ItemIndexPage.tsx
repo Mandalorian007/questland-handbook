8b388413cea74fa580cd0b0265d0b447a01c790c
mcf <mcf@spotify.com>
2020-02-12 21:48:23 -0500

    generic component for a paging and sorting table

--- src/pages/ItemIndexPage.tsx
+++ src/pages/ItemIndexPage.tsx
@@ -5,27 +5,15 @@ import {
   useSelector as useReduxSelector
 } from 'react-redux';
 import { AppState } from '../store/rootReducer';
-import {
-  Paper,
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow
-} from '@material-ui/core';
+import { Paper } from '@material-ui/core';
 import { loadItems } from '../store/itemActions';
-import TableSortLabel from '@material-ui/core/TableSortLabel';
-import TablePagination from '@material-ui/core/TablePagination';
-import { getComparator, stableSort } from '../lib/sort';
 import { Item } from '../domain/item';
+import {
+  ColumnData,
+  PagingSortingTable
+} from '../components/PagingSortingTable';
 
-interface ColumnData {
-  id: keyof Item;
-  label: string;
-}
-
-const itemColumns: ColumnData[] = [
+const itemColumns: ColumnData<Item>[] = [
   { id: 'name', label: 'Name' },
   { id: 'totalPotential', label: 'Potential' },
   { id: 'quality', label: 'Quality' },
@@ -40,26 +28,8 @@ const itemColumns: ColumnData[] = [
 export const useSelector: TypedUseSelectorHook<AppState> = useReduxSelector;
 
 export const ItemIndexPage: React.FC<{}> = () => {
-  const items = useSelector(state => state.itemState.items);
-  const [order, setOrder] = React.useState<'asc' | 'desc'>('asc');
-  const [orderBy, setOrderBy] = React.useState<keyof Item>('totalPotential');
-  const [page, setPage] = React.useState(0);
-
+  const items: Item[] = useSelector(state => state.itemState.items);
   const rowsPerPage: number = 10;
-
-  const handleRequestSort = (property: keyof Item) => {
-    const isAsc = orderBy === property && order === 'asc';
-    setOrder(isAsc ? 'desc' : 'asc');
-    setOrderBy(property);
-  };
-
-  const handleChangePage = (event: any, newPage: number) => {
-    setPage(newPage);
-  };
-
-  const emptyRows =
-    rowsPerPage - Math.min(rowsPerPage, items.length - page * rowsPerPage);
-
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -68,57 +38,12 @@ export const ItemIndexPage: React.FC<{}> = () => {
 
   return (
     <Paper>
-      <TableContainer>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              {itemColumns.map((column: ColumnData, index: number) => (
-                <TableCell
-                  key={column.id}
-                  align={index === 0 ? 'left' : 'right'}
-                >
-                  <TableSortLabel
-                    active={orderBy === column.id}
-                    direction={orderBy === column.id ? order : 'asc'}
-                    onClick={() => handleRequestSort(column.id)}
-                  >
-                    {column.label}
-                  </TableSortLabel>
-                </TableCell>
-              ))}
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {stableSort(items, getComparator<Item>(order, orderBy))
-              .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-              .map((item: any) => (
-                <TableRow key={item.id}>
-                  <TableCell>{item.name}</TableCell>
-                  <TableCell align="right">{item.totalPotential}</TableCell>
-                  <TableCell align="right">{item.quality}</TableCell>
-                  <TableCell align="right">{item.itemSlot}</TableCell>
-                  <TableCell align="right">{item.emblem}</TableCell>
-                  <TableCell align="right">{item.attack}</TableCell>
-                  <TableCell align="right">{item.magic}</TableCell>
-                  <TableCell align="right">{item.defense}</TableCell>
-                  <TableCell align="right">{item.health}</TableCell>
-                </TableRow>
-              ))}
-            {emptyRows > 0 && (
-              <TableRow style={{ height: 53 * emptyRows }}>
-                <TableCell colSpan={6} />
-              </TableRow>
-            )}
-          </TableBody>
-        </Table>
-      </TableContainer>
-      <TablePagination
-        rowsPerPageOptions={[rowsPerPage]}
-        component="div"
-        count={items.length}
+      <PagingSortingTable
+        columns={itemColumns}
+        items={items}
         rowsPerPage={rowsPerPage}
-        page={page}
-        onChangePage={handleChangePage}
+        initialSortingProperty={'totalPotential'}
+        initialSortDirection={'desc'}
       />
     </Paper>
   );
