409cfd506e8a54cf6f9686a5d11b8523dd16c29e
mcf <mcf@spotify.com>
2020-03-07 22:33:01 -0500

    initial monster slayer calculator work

---
+++ src/components/MonsterPointSelector.tsx
@@ -0,0 +1,42 @@
+import TextField from '@material-ui/core/TextField';
+import Autocomplete from '@material-ui/lab/Autocomplete';
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  root: {
+    '& .MuiTextField-root': {
+      margin: theme.spacing(1)
+    },
+    '& .MuiAutocomplete-root': {
+      margin: theme.spacing(1),
+      display: `inline-flex`
+    }
+  }
+}));
+
+export const MonsterPointSelector: React.FC<{
+  monsterNames: string[];
+}> = ({ monsterNames }) => {
+  const classes = useStyles();
+
+  return (
+    <form className={classes.root} noValidate autoComplete="off">
+      <Autocomplete
+        id="monster-selector"
+        options={monsterNames}
+        getOptionLabel={option => option}
+        style={{ width: 300, display: `inline - flex` }}
+        renderInput={params => (
+          <TextField {...params} label="Monster Selector" variant="outlined" />
+        )}
+      />
+      <TextField
+        id="standard-number"
+        label="Points"
+        type="number"
+        defaultValue={0}
+      />
+    </form>
+  );
+};
--- src/pages/tools/MonsterSlayerCalcPage.tsx
+++ src/pages/tools/MonsterSlayerCalcPage.tsx
@@ -1,23 +1,44 @@
-import React, {useEffect} from 'react';
-import {TypedUseSelectorHook, useDispatch, useSelector as useReduxSelector} from 'react-redux';
-import {AppState} from '../../store/rootReducer';
-import {Paper} from '@material-ui/core';
-import {BattleLocation} from "../../domain/battleLocation";
-import {loadBattleLocations} from "../../store/battleLocationActions";
+import React, { useEffect } from 'react';
+import {
+  TypedUseSelectorHook,
+  useDispatch,
+  useSelector as useReduxSelector
+} from 'react-redux';
+import { AppState } from '../../store/rootReducer';
+import { BattleLocation, Stage, StageMob } from '../../domain/battleLocation';
+import { loadBattleLocations } from '../../store/battleLocationActions';
+import { MonsterPointSelector } from '../../components/MonsterPointSelector';
 
 export const useSelector: TypedUseSelectorHook<AppState> = useReduxSelector;
 
+const extractMonsterNames = (battleLocations: BattleLocation[]) => {
+  const monsterNames: Set<string> = new Set();
+
+  let battleLocation: BattleLocation;
+  for (battleLocation of battleLocations) {
+    let stage: Stage;
+    for (stage of battleLocation.stages) {
+      let mob: StageMob;
+      for (mob of stage.stageMobs) {
+        monsterNames.add(mob.name);
+      }
+    }
+  }
+  return Array.from(monsterNames).sort();
+};
+
 export const MonsterSlayerCalcPage: React.FC<{}> = () => {
-    const battleLocations: BattleLocation[] = useSelector(state => state.battleLocationState.battleLocations);
-    const dispatch = useDispatch();
+  const battleLocations: BattleLocation[] = useSelector(
+    state => state.battleLocationState.battleLocations
+  );
+  const monsterNames = useSelector(state =>
+    extractMonsterNames(state.battleLocationState.battleLocations)
+  );
+  const dispatch = useDispatch();
 
-    useEffect(() => {
-        dispatch(loadBattleLocations());
-    }, [dispatch]);
+  useEffect(() => {
+    dispatch(loadBattleLocations());
+  }, [dispatch]);
 
-    return (
-        <Paper>
-            ${JSON.stringify(battleLocations)}
-        </Paper>
-    );
+  return <MonsterPointSelector monsterNames={monsterNames} />;
 };
