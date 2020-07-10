761e3ca2af58589117e2d0a9adf6c32f7e56d102
mcf <mcf@spotify.com>
2020-07-10 13:26:17 -0400

    massive improvements to the readability of the new player section on the QL Handbook!

--- src/pages/guides/BeginnerTipsPage.tsx
+++ src/pages/guides/BeginnerTipsPage.tsx
@@ -7,84 +7,84 @@ export const BeginnerTipsPage = () => (
     <h3>Push through the campaign</h3>
     <p>
       The campaign is useful for a number of reasons. As a new player it should
-      be a primary focus when starting out. The gold, experience and eternium
-      provided for each level is small, but getting the star prizes on each
-      location will provide other valuable resources and your best source of
-      gems in the beginning. The number of stars you have on the campaign also
-      increases your Idle reward generation. This is the most important factor
-      as it will be your most consistent source of several items and resources
-      going forward.
+      be your primary focus when starting out. Each level provides small amounts
+      of gold, experience, and eternium, but the best source of gems are the
+      star prizes from completing each location. Increasing the number of stars
+      you have on the campaign also increases your Idle reward generation, which
+      will be your most consistent future source of items and resources.
       <br />
       Stars also dictate which talents are obtainable from the Event Call talent
-      packs in the blimp. Unlocking the talents used in the Core/ Popular Builds
+      packs in the blimp. Unlocking the talents used in the Core/Popular Builds
       section of this handbook should be one of your first priorities starting
       out. Some of these talents require a specific star amount of the campaign.
       <br />
-      When stuck on the campaign I would recommend farming rare material levels,
-      and later essence levels. Your goal here would be to get the rare
-      materials to purchase high potential Legendary orbs in the Event Blimp
-      Trade Spot, and the essence to increase orb power. Once your hero level is
-      higher try to push the campaign levels again.
+      When stuck on the campaign, I would recommend farming rare material
+      levels, and then essence levels. Your goal here would be to get the rare
+      materials for purchasing high potential Legendary orbs in the Event Blimp
+      Trade Spot, and essences to increase orb power. Once your hero level is
+      higher, try to push the campaign levels again.
     </p>
     <h3>Gem Spending</h3>
     <p>
-      Some of the best uses of gems when starting out is to get the core talents
-      you will need in the game. You can find talent packs in the Event Call
-      section of the Event Blimp. They cost 500 gems and you are guaranteed to
-      get the promoted talent on the page, plus 1 or 2 additional talents that
-      you have enough campaign stars to get. When trying for something specific
-      check the rewards button to see what talents are even possible for you,
-      you may need to advance farther in the campaign. The promoted talent
-      changes twice a week, so keep an eye out for talents that you need. I
-      would recommend buying a few packs to start out and then a single pack
-      later when you are missing the promoted talent. The price of Talent packs
-      increase after 5 packs bought, If you're trying to conceive gems I would
+      Some of the best uses of gems when starting out are the core talents you
+      will need in the game. You can find talent packs in the Event Call section
+      of the Event Blimp. They cost 500 gems and you are guaranteed to get the
+      promoted talent on the page, plus 1 or 2 additional talents that your
+      number of campaign stars qualify for. When trying for something specific,
+      check the rewards button to see what talents are available for you. You
+      may need to advance farther in the campaign. The promoted talent changes
+      twice a week, so keep an eye out for talents that you need. I would
+      recommend buying a few packs to start out and then a single pack later
+      when you are missing the promoted talent. The price of Talent packs
+      increase after 5 packs bought, If you're trying to conserve gems I would
       stop when the price increases. The price will go back down when the next
       talent replaces it.
       <br />
       Your second best use of gems will be buying some of the smaller Quest
-      Event used items in The Mall. This includes gear schemes, eternal keys,
-      and spin tokens. Your goal when buying these is to increase your stock to
-      be able to complete the weekly Quest Events for more rewards, this will be
-      talked about later. When it comes to spending gems to acquire legendaries,
-      the most cost effective way will be buying 1 gear scheme a day, and avoid
-      the current promoted chests. The chests cost 300-500 gems and gives a 5%
-      chance for a legendary with a guaranteed 1 legendary if you buy 10. Gear
-      schemes in the mall cost 1000 gems and give you a legendary of the emblem
-      of your choice.
+      Event items from the Mall. This includes gear schemes, eternal keys, and
+      spin tokens. Your goal when buying these is to increase your stock to
+      complete the weekly Quest Events to earn more rewards. This will be talked
+      about later.
+      <br />
+      When it comes to spending gems to acquire legendaries, the most cost
+      effective way is to buy 1 gear scheme a day, and avoid the current
+      promoted chests. The chests cost 300-500 gems and give a 5% chance for a
+      legendary with a guaranteed 1 legendary if you buy 10. Gear schemes in the
+      Mall cost 1000 gems and give you a legendary of the emblem of your choice.
     </p>
     <h3>Build setup</h3>
     <p>
-      The best advice for having the most effective build is to go to the Core/
-      Popular Builds section and follow setups you see there. Because these
-      builds are so needed and you could even say overpowered, they have made
-      the weapons very rare and difficult to obtain. So for new players, or
-      those who don't have these weapons and talents battling can be more
-      difficult, but there alternatives that can help you be successful. It's
-      too difficult to name every alternate build so I will talk about things to
+      The best advice for having the most effective build is to go to the
+      Core/Popular Builds section and follow setups you see there. Because these
+      builds are so necessary -- and you could even say overpowered-- they have
+      made the weapons very rare and difficult to obtain. So for new players, or
+      those who don't have these weapons and talents, battling can be more
+      difficult, but there are alternatives that can help you succeed. It's too
+      difficult to name every alternate build so I will talk about things to
       look for.
       <br />
-      First off look for weapons that do one of three main things: add spirits
+      First off, look for weapons that do one of three main things: add spirits
       to your board, chain other talents, and lastly provide health. Adding
-      spirits is done in Meta build by weapons like Hecatombus and Malachite
-      Truncheon that spawn 2 spirits off heals. For an alternative weapon you
-      can look for weapons like Thunderblade, that spawns red for opponents
-      magic attack, or Banshee Autumn that spawns 1 blue every turn your health
-      is high enough, even weapons like Chosen One that add spirits only one
-      time could be useful in the right situation. There are both main hand and
-      offhand weapons that can help you control the spirits on your board.
+      spirits is done in a meta build by weapons like Hecatombus and Malachite
+      Truncheon that spawn 2 spirits off heals. For an alternative weapon, you
+      can look for weapons like Thunderblade, that spawns red spirits for an
+      opponent’s magic attack, or Banshee Autumn that spawns 1 blue every turn
+      your health is high enough. Even weapons like Chosen One that add spirits
+      only one time could be useful in the right situation. There are both main
+      hand and offhand weapons that can help you control the spirits on your
+      board.
       <br />
       Chaining weapons are key to creating the synergy with your talents and are
       used in all meta builds. For example the Ratchet Rush build uses two
       chaining weapons, Ratchet Hatchet and Azazel Shield. This creates a system
-      that whenever 4 red spirits are used each the weapons passives activate to
-      trigger the 1 white and 1 blue spirits. This gives the build the ability
-      to generate 3 red spirits each time that 4 red spirits are used to keep
-      your damage going. There are currently a few main hand and several offhand
-      options for these weapons so check your index and see if you have one to
-      use.
+      so whenever 4 red spirits are used, each of the weapons passive effects
+      activate to trigger 1 white and 1 blue spirits. This gives the build the
+      ability to generate 3 red spirits each time that 4 red spirits are used to
+      keep your damage going. There are currently a few main hand and several
+      offhand options for these weapons so check your index and see if you have
+      one to use.
       <br />
-      In the event that you have neither of these weapon types your next best
+      In the event that you have neither of these weapon types, your next best
       solution would be to look for survivability. In my opinion weapons that
       provide sustainable healing are good alternatives. Items like Sabertooth,
       The Strix, and Iron Bat can get you health for spirit usage and could keep
@@ -93,7 +93,7 @@ export const BeginnerTipsPage = () => (
     <h3>Starting to gear</h3>
     <p>
       For more in depth review of gear, linking bonuses, stats to pick,
-      awakening and collection setup look in the Gearing section in the
+      awakening and collection setup, look in the Gearing section in the
       handbook.
       <br />
       As a beginner player just know that your biggest power increase will be
@@ -101,13 +101,15 @@ export const BeginnerTipsPage = () => (
       So starting out your focus should be on collecting and saving as many
       legendary gear pieces as possible. It's not worth it to smelt down or sell
       older/weaker legendary items as they will be useful as you progress
-      further. When you choose your starting gear look for gear with higher
-      potential and start trying to collect gear from the same set to start
-      activating bonus links.
+      further.
+      <br />
+      When you choose your starting gear, look for gear with higher potential
+      and start trying to collect gear from the same set to start activating
+      bonus links.
       <br />
       While awakening is going to be your biggest power increase, don't jump to
       do so right away. It's best to have an idea of the gear you are going to
-      be using and your collection setup before you invest too heavily. Read
+      be using and your collection set up before you invest too heavily. Read
       more about awakening in the guides section.
     </p>
     <h3>Hoarding resources</h3>
@@ -115,40 +117,42 @@ export const BeginnerTipsPage = () => (
       If you talk to any long time players the first thing most will tell you is
       to hoard all your resources. With the way the current weekly Quest Events
       are set up, most resources can get you additional rewards if they are used
-      during their respective Quest Events. The current quest events require the
-      use of: spin tokens, arena tickets, eternal keys, gear schemes, legendary
-      shards. All of these items should be held onto until their quest event to
-      get the most rewards. The only exception for this is, new players should
-      use their gear schemes and legendary shards to get their gear and
-      collections outfitted first, before hoarding.
+      during their respective Quest Events. The current Quest Events require the
+      use of: spin tokens, arena tickets, eternal keys, gear schemes, and
+      legendary shards. All of these items should be kept until their Quest
+      Event to get the most rewards. The only exception for this is -- new
+      players should use their gear schemes and legendary shards to get their
+      gear and collections outfitted first, before hoarding.
       <br />
       Other items that are worth saving that aren’t used in the current Quest
-      Event are: Energy Stews, Loot tokens, Legendary orb keys, Gold and silver
-      keys, and craft tokens. Energy stews are worth saving to be able to
-      complete the Monster Slayer quests. All the other items are resources that
-      had Quest Events for them at one point before and could come back, so it's
-      worth having a small stock saved for them. Legendary orb keys should be
-      used by new players until you get all your orb slots legendary.
+      Event are: Loot tokens, Legendary orb keys, gold and silver keys, and
+      craft tokens. Energy stews are worth saving for completing the Monster
+      Slayer quests. All the other items are resources that had Quest Events for
+      them at one point before and could come back, so it's worth saving.
+      Legendary orb keys should be used by new players until you get all your
+      orb slots legendary.
     </p>
     <h3>Collection Scrolls</h3>
     <p>
       These items are used to unlock and then upgrade your collection slots. The
-      biggest tip for new players is to make sure you level up your character
-      and unlock all the slots before you upgrade any of the slots. This will
-      require getting to level 67. Each slot costs 5 scrolls to unlock, so you
-      will need 50 of each type to fully unlock your collections.
+      biggest tip for new players is to level up your character to level 67 and
+      unlock all the slots before upgrading any of the slots. Each slot costs 5
+      scrolls to unlock, so you will need 50 of each type to fully unlock your
+      collections.
       <br />
-      There are differing opinions on how to upgrade up your slots. The most
-      basic is to level them all evenly, this is good for balance and to
-      increase your hero power. Others will focus on health slots as health is
-      one of the most important stats for boss battle in the game. Another
-      option is to focus on the slots with your higher potential items for the
-      most boost, this is good for quickly raising your hero power, but your
-      collections are then harder to change around items. If you're going to
-      focus on specific slots be aware that scrolls become less effective after
-      some specific percentages: 100, 190, 220, and 240%. It would be worthwhile
-      once you hit these levels to bring the rest of your collection slots up as
-      well to make the most out of your early scrolls.
+      There are differing opinions on how to best upgrade your slots.
+      <br />
+      The most basic is to level them all evenly, which is good for balance and
+      increasing your hero power. Others will focus on upgrading health slots
+      because health is one of the most important stats for boss battles in the
+      game. Another option is to focus on the slots with your higher potential
+      items for the most immediate boost. This is good for quickly raising your
+      hero power, but your collections are then harder to change around.
+      <br />
+      If you're going to focus on specific slots, be aware that scrolls become
+      less effective after some specific percentages: 100, 190, 220, 240, and
+      275%. It would be worthwhile once you hit these levels to upgrade the
+      remaining collection slots to maximize your early scrolls.
     </p>
     <h3>Using resources</h3>
     <h4>Gold</h4>
@@ -156,37 +160,40 @@ export const BeginnerTipsPage = () => (
       As a new player your primary use for gold is to upgrade your orbs. The
       gold you invest in your orbs is nonrefundable, so I would caution against
       upgrading anything less than a legendary orb, and only max level 100 high
-      potential orbs. As you start to outfit your orb slots and you have some
-      excess gold, one good use is some of the items in the mall to stock up for
-      quest events. Your end game use for gold will be reforging all your gear.
-      However you shouldn’t start that process until you get all your gear and
-      collections sorted out and you know what your final setup is most likely
-      going to be, since reforging is such a resource pit. The item shop should
-      be avoided as a new player since there are better uses for your gold then
-      buying low quality items for smelting or crafting.
+      potential orbs.
+      <br />
+      As you start to outfit your orb slots and have some excess gold, one good
+      use is to buy some of the items in the Mall to stock up for Quest Events.
+      Your end game use for gold will be reforging all your gear. However you
+      shouldn’t start reforging until you’ve sorted out all of your collections
+      and decided on a final gear set since reforging is such a resource pit.
+      <br />
+      The item shop should be avoided as a new player since there are better
+      uses for your gold then buying low quality items for smelting or crafting.
     </p>
     <h4>Craft Tokens</h4>
     <p>
       In the most recent 3.0 update, craft tokens don’t have as much use as they
-      did before. As a new player however best use for them would be for
-      Legendary shards to build up your gear. And then eternal keys for the
-      Quest event.
+      did before. As a new player however, the best use for them would be for
+      crafting Legendary shards to build up your gear, and then creating eternal
+      keys for the Quest Event.
     </p>
     <h4>Barrels</h4>
     <p>
-      Barrels are a great steady income and the barrel shop will be your best
-      daily source of your hoarded quest event resources. If you can keep the
-      voyages rolling and get the bonus for completing all of them in a set, not
-      only will you get a steady source gold and eternium, but eternal keys as
-      well. The best use of barrels is purchasing your Quest Event items:
-      eternal keys, arena tickets, spin tokens. As a newer player it's
-      worthwhile to buy the single of each scroll, (the x2 scrolls price isn't
-      worth it) until you get your collections unlocked. I would even keep
-      buying until the collection upgrade cost is enough that one scroll isn’t
-      beneficial. If you can keep your voyages going throughout the day you
-      could average 50+ a day, more than enough to purchase all the above. You
-      can then use the surplus to purchase a Legendary gear scheme every few
-      days, and loot tokens and energy stews as needed.
+      Barrels are a great source of steady income and the barrel shop will be
+      your best daily source of your hoarded Quest Event resources. If you can
+      get the bonus for completing all voyages, not only will you get a steady
+      source of gold and eternium, but eternal keys as well. The best use of
+      barrels is purchasing your Quest Event items: eternal keys, arena tickets,
+      and spin tokens.
+      <br />
+      As a newer player, it's worthwhile to buy the single of each scroll (the
+      x2 scrolls price isn't worth it) until your collections are unlocked. I
+      would even keep buying until the collection upgrade cost is enough that
+      one scroll isn’t beneficial. If you can keep your voyages going throughout
+      the day you could average 50+ a day, more than enough to purchase all the
+      above. You can then use the surplus barrels to purchase a Legendary gear
+      scheme every few days, and loot tokens and energy stews as needed.
     </p>
     <h4>Stamps</h4>
     <p>
