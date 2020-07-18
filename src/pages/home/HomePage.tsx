d93f4f74794ebd2541707a6d7b47bd0822b31260
mcf <mcf@spotify.com>
2020-07-18 00:15:17 -0400

    update my acknowledgements

--- src/pages/home/HomePage.tsx
+++ src/pages/home/HomePage.tsx
@@ -23,9 +23,18 @@ export const HomePage = () => {
 
       <h3>Special Thanks</h3>
       <p>
-        This document is only possible because of the many amazing guild members
-        and friends who have taught me. I want to call out some in particular
-        who really inspired me with how helpful and amazing they have been.
+        The Questland Handbook project was only made possible because of some
+        amazing people who have seriously helped me along the way. And I want to
+        take a moment to acknowledge these incredible people who have taken
+        their time to make this possible.
+      </p>
+      <h4>My Mentors</h4>
+      <p>
+        The folks in this section taught so much about Questland that I could
+        not have provided so much content to all of you without. In addition
+        many of these members built the foundations of an incredible community
+        to share knowledge and have fun together. I hope this guide can help
+        bring that to you and your own communities.
       </p>
       <ul>
         <li>SIBB</li>
@@ -34,8 +43,28 @@ export const HomePage = () => {
         <li>Punisher777</li>
         <li>Draugluin</li>
         <li>Juoh</li>
-        <li>Monstercat</li>
         <li>Peitcadon</li>
+      </ul>
+      <h4>Guide contributors</h4>
+      <p>
+        These incredible people are the members that their content to be
+        re-published here for the betterment of the Questland community. Many
+        are excluded here due to privacy requests, but know that so many people
+        collaborated to make this possible!
+      </p>
+      <ul>
+        <li>SIBB</li>
+        <li>Punisher777</li>
+        <li>Edo</li>
+      </ul>
+      <h4>Content Editors</h4>
+      <p>
+        These are the community members who take my terribly writing skills and
+        make it easy to understand and access for everyone here. Without their
+        help I would still be second guessing phrasing for months!
+      </p>
+      <ul>
+        <li>Ankari</li>
         <li>ZombieKat</li>
       </ul>
 
