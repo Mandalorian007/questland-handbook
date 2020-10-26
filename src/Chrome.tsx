d63ff4048e312e4019925fdb1a01737e490237f7
Mandalorian007 <mandalorian007@gmail.com>
2020-10-26 16:09:10 -0400

    sticky Patreon Footer

--- src/Chrome.tsx
+++ src/Chrome.tsx
@@ -8,138 +8,162 @@ import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import {
-  createMuiTheme,
-  createStyles,
-  makeStyles,
-  Theme,
-  ThemeProvider
-} from '@material-ui/core/styles';
-import { Paper } from '@material-ui/core';
-import { NavItemGroup, RoutableNavList } from './components/RoutableNavList';
+import {createMuiTheme, createStyles, makeStyles, Theme, ThemeProvider} from '@material-ui/core/styles';
+import {Container, Grid, Paper} from '@material-ui/core';
+import {NavItemGroup, RoutableNavList} from './components/RoutableNavList';
 
 const darkTheme = createMuiTheme({
-  palette: {
-    type: 'dark'
-  }
+    palette: {
+        type: 'dark'
+    }
 });
 
 const drawerWidth = 240;
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    root: {
-      display: 'flex'
-    },
-    drawer: {
-      [theme.breakpoints.up('sm')]: {
-        width: drawerWidth,
-        flexShrink: 0
-      }
-    },
-    appBar: {
-      [theme.breakpoints.up('sm')]: {
-        width: `calc(100% - ${drawerWidth}px)`,
-        marginLeft: drawerWidth
-      }
-    },
-    menuButton: {
-      marginRight: theme.spacing(2),
-      [theme.breakpoints.up('sm')]: {
-        display: 'none'
-      }
-    },
-    toolbar: theme.mixins.toolbar,
-    drawerPaper: {
-      width: drawerWidth
-    },
-    content: {
-      flexGrow: 1,
-      padding: theme.spacing(3)
-    },
-    lists: {
-      backgroundColor: theme.palette.background.paper,
-      marginTop: theme.spacing(1)
-    },
-    nested: {
-      paddingLeft: theme.spacing(4)
-    }
-  })
+    createStyles({
+        root: {
+            display: 'flex',
+        },
+        drawer: {
+            [theme.breakpoints.up('sm')]: {
+                width: drawerWidth,
+                flexShrink: 0
+            }
+        },
+        appBar: {
+            [theme.breakpoints.up('sm')]: {
+                width: `calc(100% - ${drawerWidth}px)`,
+                marginLeft: drawerWidth
+            }
+        },
+        menuButton: {
+            marginRight: theme.spacing(2),
+            [theme.breakpoints.up('sm')]: {
+                display: 'none'
+            }
+        },
+        toolbar: theme.mixins.toolbar,
+        drawerPaper: {
+            width: drawerWidth
+        },
+        lists: {
+            backgroundColor: theme.palette.background.paper,
+            marginTop: theme.spacing(1)
+        },
+        nested: {
+            paddingLeft: theme.spacing(4)
+        },
+        contentContainer: {
+            display: 'flex',
+            minHeight: '100vh',
+            flexDirection: 'column',
+        },
+        content: {
+            flex: 1,
+            padding: theme.spacing(3),
+        },
+        footer: {
+            padding: theme.spacing(0),
+            paddingTop: theme.spacing(3),
+            paddingBottom: theme.spacing(0),
+        },
+        footerContent: {
+            padding: theme.spacing(2)
+        }
+    })
 );
 
 export const Chrome: React.FC<{
-  title: string;
-  navItemGroups: NavItemGroup[];
-}> = ({ title, navItemGroups, children }) => {
-  const classes = useStyles();
-  const [mobileOpen, setMobileOpen] = React.useState(false);
+    title: string;
+    navItemGroups: NavItemGroup[];
+}> = ({title, navItemGroups, children}) => {
+    const classes = useStyles();
+    const [mobileOpen, setMobileOpen] = React.useState(false);
 
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
+    const handleDrawerToggle = () => {
+        setMobileOpen(!mobileOpen);
+    };
 
-  return (
-    <ThemeProvider theme={darkTheme}>
-      <div className={classes.root}>
-        <CssBaseline />
-        <AppBar position="fixed" className={classes.appBar}>
-          <Toolbar>
-            <IconButton
-              color="inherit"
-              aria-label="open drawer"
-              edge="start"
-              onClick={handleDrawerToggle}
-              className={classes.menuButton}
-            >
-              <MenuIcon />
-            </IconButton>
-            <Typography variant="h6" noWrap>
-              {title}
-            </Typography>
-          </Toolbar>
-        </AppBar>
-        <nav className={classes.drawer} aria-label="mailbox folders">
-          {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
-          <Hidden smUp implementation="css">
-            <Drawer
-              variant="temporary"
-              anchor="left"
-              open={mobileOpen}
-              onClose={handleDrawerToggle}
-              classes={{
-                paper: classes.drawerPaper
-              }}
-              ModalProps={{
-                keepMounted: true // Better open performance on mobile.
-              }}
-            >
-              <Paper>
-                <div className={classes.toolbar} />
-                <Divider />
-                <RoutableNavList navItemGroups={navItemGroups} />
-              </Paper>
-            </Drawer>
-          </Hidden>
-          <Hidden xsDown implementation="css">
-            <Drawer
-              classes={{
-                paper: classes.drawerPaper
-              }}
-              variant="permanent"
-              open
-            >
-              <Paper>
-                <div className={classes.toolbar} />
-                <RoutableNavList navItemGroups={navItemGroups} />
-              </Paper>
-            </Drawer>
-          </Hidden>
-        </nav>
-        <main className={classes.content}>
-          <div className={classes.toolbar} />
-          {children}
-        </main>
-      </div>
-    </ThemeProvider>
-  );
+    return (
+        <ThemeProvider theme={darkTheme}>
+            <div className={classes.root}>
+                <CssBaseline/>
+                <AppBar position="fixed" className={classes.appBar}>
+                    <Toolbar>
+                        <IconButton
+                            color="inherit"
+                            aria-label="open drawer"
+                            edge="start"
+                            onClick={handleDrawerToggle}
+                            className={classes.menuButton}
+                        >
+                            <MenuIcon/>
+                        </IconButton>
+                        <Typography variant="h6" noWrap>
+                            {title}
+                        </Typography>
+                    </Toolbar>
+                </AppBar>
+                <nav className={classes.drawer} aria-label="mailbox folders">
+                    {/* The implementation can be swapped with js to avoid SEO duplication of links. */}
+                    <Hidden smUp implementation="css">
+                        <Drawer
+                            variant="temporary"
+                            anchor="left"
+                            open={mobileOpen}
+                            onClose={handleDrawerToggle}
+                            classes={{
+                                paper: classes.drawerPaper
+                            }}
+                            ModalProps={{
+                                keepMounted: true // Better open performance on mobile.
+                            }}
+                        >
+                            <Paper>
+                                <div className={classes.toolbar}/>
+                                <Divider/>
+                                <RoutableNavList navItemGroups={navItemGroups}/>
+                            </Paper>
+                        </Drawer>
+                    </Hidden>
+                    <Hidden xsDown implementation="css">
+                        <Drawer
+                            classes={{
+                                paper: classes.drawerPaper
+                            }}
+                            variant="permanent"
+                            open
+                        >
+                            <Paper>
+                                <div className={classes.toolbar}/>
+                                <RoutableNavList navItemGroups={navItemGroups}/>
+                            </Paper>
+                        </Drawer>
+                    </Hidden>
+                </nav>
+                <Container className={classes.contentContainer}>
+                    <Container component="main" className={classes.content}>
+                        <div className={classes.toolbar}/>
+                        {children}
+                    </Container>
+                    <Container component="footer" className={classes.footer}>
+                        <Paper>
+                            <Grid container direction="row" alignItems="center" className={classes.footerContent}>
+                                <Typography variant="body1">Learn more about Questland at my Patreon{`\xa0\xa0`}</Typography>
+                                <a href="https://www.patreon.com/bePatron?u=33921067" target="_blank"
+                                   rel="noopener noreferrer">
+                                    <img
+                                        src="become_a_patron_button@2x.png"
+                                        alt="unable to load patreon logo"
+                                        width={160}
+                                    />
+                                </a>
+                            </Grid>
+                        </Paper>
+                    </Container>
+                </Container>
+            </div>
+        </ThemeProvider>
+    );
 };
